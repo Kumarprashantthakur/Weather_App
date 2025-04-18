87081ff8c90c3d207c4f88d128b478dde9c39682
Prashant Thakur <thakurprashant123@gmail.com>
2025-04-18 22:52:07 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import WeatherApp from "./WeatherApp"
 import "./App.css";
-import Navbar from "./Navbar";
 import viedoBg from "./assets/vedio2.mp4"
 import React from "react";
 function App() {
@@ -10,8 +9,6 @@ function App() {
     <div className="app-container">
      <video autoPlay muted loop className="background-video">
         <source src={viedoBg} type="video/mp4" /></video>
-        <Navbar/>
-        <br />
         <div className="content">
      <WeatherApp/>
      </div>
